cfab323fd871783730240c3f05ef48b84d490f4d
sssheli <s10272432@connect.np.edu.sg>
2025-07-28 17:50:49 +0800

    this is my first time using github

---
+++ babyshark.py
@@ -0,0 +1,8 @@
+message = 'Baby Shark do do do' 
+print(len(message))
+print(message.find('do'))
+print(message.find( 'do', 0, 12))
+print(message.replace('o ', 'ie'))
+print(message[5:10].isalpha())
+print('shark' in message)
+print(message[22])
